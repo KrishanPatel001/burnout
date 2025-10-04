165875bb8676fa6084e85d1bdeab4f62ec66738d
Krishan Patel <krishanpatel@Krishans-MacBook-Pro.local>
2025-10-03 20:33:12 -0400

    Added base for first level and player sprite, need to fix movement

---
+++ Scripts/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+@export var moveSpeed : float = 100
+@export var acceleration : float = 50
+@export var braking : float = 20
+@export var gravity : float = 500
+@export var jumpForce : float = 200
+
+var moveInput : float
+
+@onready var sprite : Sprite2D = $Sprite
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	moveInput = Input.get_axis("move_left", "move_right")
+	
+	if moveInput != 0:
+		velocity.x = lerp(velocity.x, moveInput * moveSpeed, acceleration * delta)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, braking * delta)
+		
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = -jumpForce
+		
+	move_and_slide()
